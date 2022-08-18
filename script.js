a6550ea141d143823fd44b2c2fd453495ba3f994
Gar-Yin Lam <garyin.lam@gmail.com>
2022-08-18 16:07:53 +0100

    working on desktop view with history

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+const numbers = document.querySelectorAll(".button-container__button--number");
+const display = document.getElementById("display-box");
+const result = document.getElementById("result-box");
+const clear = document.getElementsByClassName("button-container__button--clear")[0];
+const brackets = document.querySelector(".brackets").children;
+
+//operators and their associated typed version
+const add = document.getElementById("add");
+add.operator = "+";
+const minus = document.getElementById("minus");
+minus.operator = "-";
+const multiply = document.getElementById("multiply");
+multiply.operator = "*";
+const divide = document.getElementById("divide");
+divide.operator = "/";
+
+const prev = document.getElementById("prev");
+const equals = document.getElementById("equals");
+const back = document.getElementById("back");
+
+const history = document.getElementById("history");
+
 let prevAns = 0;
+
+//evaluate the given equation
 const evaluateEquation = (equation) => {
+  //check for brackets first
   if(equation.indexOf("(") != -1) {
+    //find corresponding closing bracket
     const start = equation.indexOf("(");
     let end = start;
     let bracketLevel = 0;
     for (let i = start+1; i < equation.length; i++){
+      //if found an opening bracket first then ignore next closing bracket
       if(equation.charAt(i) == "(") {
         bracketLevel++;
       } else if (equation.charAt(i) == ")" && bracketLevel > 0){
         bracketLevel--;
       } else if (equation.charAt(i) == ")" && bracketLevel == 0) {
         end = i;
+        //stop loop once matching bracket is found
         i = equation.length
       }
     }
+    // recursive call, evaluate brackets first then put answer back in equation
     return evaluateEquation(equation.slice(0,start) + evaluateEquation(equation.slice(start+1,end)) + equation.slice(end+1));
+  //look for +/- first so * and / evaluate first
   } else if (equation.indexOf("+") != -1) {
     const operatorPos = equation.lastIndexOf("+");
     return evaluateEquation(equation.slice(0,operatorPos)) + evaluateEquation(equation.slice(operatorPos+1));
@@ -27,33 +57,67 @@ const evaluateEquation = (equation) => {
   } else if (equation.indexOf("*") != -1) {
     const operatorPos = equation.lastIndexOf("*");
     return evaluateEquation(equation.slice(0,operatorPos)) * evaluateEquation(equation.slice(operatorPos+1));
+  // not operator, return number
   } else {
     return Number(equation);
   }
 }
 
+//methods used for event listeners
 const writeCurrent = (button) => {
-  document.getElementById("display-box").innerHTML += button;
+  display.innerHTML += button;
 }
 
 const displayAnswer = () => {
-  const equation = document.getElementById("display-box").innerHTML;
+  const equation = display.innerHTML;
   const answer = evaluateEquation(equation);
-  prevAns = Number(answer.toFixed(10))
-  document.getElementById("result-box").innerHTML = prevAns;
+  prevAns = Number(answer.toFixed(100));
+  history.innerHTML += `<p>${equation}<br>= ${prevAns}</p>`;
+  result.innerHTML = prevAns;
 }
 
 const clearCurrent = () => {
-  document.getElementById("display-box").innerHTML = "";
-  document.getElementById("result-box").innerHTML = "";
+  display.innerHTML = "";
+  result.innerHTML = "";
 }
 
 const backspace = () => {
-  let output = document.getElementById("display-box").innerHTML;
+  let output = display.innerHTML;
   output = output.slice(0,-1);
-  document.getElementById("display-box").innerHTML = output;
+  display.innerHTML = output;
 }
 
 const previousAnswer = () => {
-  document.getElementById("display-box").innerHTML += prevAns;
+  display.innerHTML += prevAns;
 }
+
+//add event listeners
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => writeCurrent(e.target.innerHTML));
+});
+
+clear.addEventListener("click", clearCurrent);
+
+for (let index = 0; index < brackets.length; index++) {
+  brackets[index].addEventListener("click", (e) => writeCurrent(e.target.innerHTML));
+}
+
+add.addEventListener("click", (e) => writeCurrent(e.target.operator));
+minus.addEventListener("click", (e) => writeCurrent(e.target.operator));
+divide.addEventListener("click", (e) => writeCurrent(e.target.operator));
+multiply.addEventListener("click", (e) => writeCurrent(e.target.operator));
+
+prev.addEventListener("click", previousAnswer);
+equals.addEventListener("click", displayAnswer);
+back.addEventListener("click", backspace);
+
+
+history.addEventListener("click", (e) => {
+  if (e.target.tagName === "P") {
+    const parts = e.target.innerHTML.split(" ");
+    prevAns = Number(parts[1]);
+    display.innerHTML = parts[0].slice(0,-5);
+    result.innerHTML = prevAns;
+  }
+})
